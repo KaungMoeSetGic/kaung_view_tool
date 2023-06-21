22613132ce4e97f034d02dacb3b4c02b4cfc833a
KaungMoeSetGic <kaungmoeset@gicjp.com>
2023-06-21 15:40:34 +0630

    Dev tool initial commit

---
+++ lib/kaung_view_tool.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kaung_view_tool/version"
+require_relative "kaung_view_tool/renderer"
+
+module KaungViewTool
+  class Error < StandardError; end
+end
---
+++ lib/kaung_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KaungViewTool
+  VERSION = "0.1.0"
+end
